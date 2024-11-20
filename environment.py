1ae9ef524cd1965d79f00ea5396b932ba8962e20
ManueLucas <m.lucas2603@gmail.com>
2024-11-19 21:27:31 -0500

    modified render function, environment now tracks which agents die in each team

--- environment.py
+++ environment.py
@@ -33,6 +33,19 @@ class VipGame(gym.Env):
         self.max_timesteps = max_timesteps
         self.timesteps_elapsed = 0
         
+        #we keep separate and redundant counts for the sake of statistics
+        self.number_of_vip_dead = 0
+        self.number_of_defender_dead = 0
+        self.number_of_attacker_dead = 0
+        self.number_of_vips = 0
+        self.number_of_attackers = 0
+        self.number_of_defenders = 0
+        
+        #which agents are still alive?
+        self.live_vips = []
+        self.live_attackers = []
+        self.live_defenders = []
+        
         self.attacker_defender_action_space = 8
         self.vip_action_space = 4
         
@@ -71,12 +84,19 @@ class VipGame(gym.Env):
             for j in range(self.grid_width):
                 if self.grid[i, j] == VIP:
                     self.vip_positions.append((i, j))  # VIP position
+                    self.number_of_vips += 1
                 elif self.grid[i, j] == DEFENDER:
                     self.defender_positions.append((i, j))  # Defender position
+                    self.number_of_defenders += 1
                 elif self.grid[i, j] == ATTACKER:
                     self.attacker_positions.append((i, j))  # Attacker position
+                    self.number_of_attackers += 1
+        self.live_vips = [True] * self.number_of_vips
+        self.live_attackers = [True] * self.number_of_attackers
+        self.live_defenders = [True] * self.number_of_defenders
 
-    def _move_agent(self, position, action, agent_type):
+
+    def _move_agent(self, position, action, agent_type, agent_id):
         # do nothing if the agent is dead
         if position == self.dead_cell:
             return position, 0
@@ -101,6 +121,7 @@ class VipGame(gym.Env):
                 # Move VIP to the death cell
                 vip_index = self.vip_positions.index(new_position)
                 self.vip_positions[vip_index] = self.dead_cell
+                self.number_of_vip_dead += 1
                 return new_position, 2  # median reward for killing VIP
             
             # when a defender and an attacker meet each other, both die
@@ -108,10 +129,14 @@ class VipGame(gym.Env):
                 # remove them from the grid render
                 self.grid[position] = 0
                 self.grid[new_position] = 0
-                
+                self.number_of_attacker_dead += 1
+                self.number_of_defender_dead += 1
+                print("Attacker and Defender killed each other")
                 if agent_type == ATTACKER:
                     # Move defender to the death cell
                     defender_index = self.defender_positions.index(new_position)
+                    self.live_defenders[defender_index] = False
+                    self.live_attackers[agent_id] = False
                     self.defender_positions[defender_index] = self.dead_cell
                     # Move attacker to the death cell
                     return self.dead_cell, 3  # high reward for killing defender
@@ -119,6 +144,8 @@ class VipGame(gym.Env):
                 elif agent_type == DEFENDER:
                     # Move attacker to the death cell
                     attacker_index = self.attacker_positions.index(new_position)
+                    self.live_defenders[agent_id] = False
+                    self.live_attackers[attacker_index] = False
                     self.attacker_positions[attacker_index] = self.dead_cell
                     # Move defender to the death cell
                     return self.dead_cell, 3 # high reward for killing attacker
@@ -134,17 +161,17 @@ class VipGame(gym.Env):
 
     def attacker_move(self, action, agent_id):
         # Update attacker position and get reward for the move
-        self.attacker_positions[agent_id], reward = self._move_agent(self.attacker_positions[agent_id], action, ATTACKER)
+        self.attacker_positions[agent_id], reward = self._move_agent(self.attacker_positions[agent_id], action, ATTACKER, agent_id)
         return reward
 
     def defender_move(self, action, agent_id): # we can merge the attacker and defender move functions into one later
         # Update defender position and get reward for the move
-        self.defender_positions[agent_id], reward = self._move_agent(self.defender_positions[agent_id], action, DEFENDER)
+        self.defender_positions[agent_id], reward = self._move_agent(self.defender_positions[agent_id], action, DEFENDER, agent_id)
         return reward
 
     def vip_move(self, action, agent_id):
         # Update VIP position and get reward for the move
-        self.vip_positions[agent_id], reward = self._move_agent(self.vip_positions[agent_id], action, VIP)
+        self.vip_positions[agent_id], reward = self._move_agent(self.vip_positions[agent_id], action, VIP, agent_id)
         return reward
 
     def step(self, actions): #we expect a tuple of 3 lists of actions, one for each team
@@ -156,22 +183,39 @@ class VipGame(gym.Env):
         vip_reward = []
         # Execute each agent's move and get their respective rewards
         for i, attacker_action in enumerate(attacker_actions):
-            attacker_reward.append(self.attacker_move(attacker_action, i))
+            if self.live_attackers[i]: # only allow the agent to move if they are alive
+                attacker_reward.append(self.attacker_move(attacker_action, i))
+            else:
+                attacker_reward.append(0)
             
         for i, defender_action in enumerate(defender_actions):
-            defender_reward.append(self.defender_move(defender_action, i))
+            if self.live_defenders[i]:
+                defender_reward.append(self.defender_move(defender_action, i))
+            else:
+                defender_reward.append(0)
             
         for i, vip_action in enumerate(vip_actions):  
-            vip_reward.append(self.vip_move(vip_action, i))
-
+            if self.live_vips[i]:
+                vip_reward.append(self.vip_move(vip_action, i))
+            else:
+                vip_reward.append(0)
         # Increment the timestep counter
         self.timesteps_elapsed += 1
         # Check if the maximum number of timesteps has been reached
-        done = self.timesteps_elapsed >= self.max_timesteps
+        truncated = self.timesteps_elapsed >= self.max_timesteps
+        terminated = False
+        # Check if the game is over (either the VIP is dead or all attackers are dead)
+        if self.number_of_vip_dead == self.number_of_vips:
+            terminated = True
+            attacker_reward = [x + 3 for x in attacker_reward] #give entire team a huge reward
+        elif self.number_of_attacker_dead == self.number_of_attackers:
+            terminated = True
+            defender_reward = [x + 3 for x in defender_reward]
+            vip_reward = [x + 3 for x in vip_reward] 
         
         defenderside_vision = self.line_of_sight(self.defender_positions + self.vip_positions)
         attackerside_vision = self.line_of_sight(self.attacker_positions)
-        return self.grid, (defenderside_vision, attackerside_vision), (defender_reward, attacker_reward, vip_reward), (self.defender_positions, self.attacker_positions, self.vip_positions), done
+        return self.grid, (defenderside_vision, attackerside_vision), (defender_reward, attacker_reward, vip_reward), (self.defender_positions, self.attacker_positions, self.vip_positions), truncated, terminated
     
     def line_of_sight(self, agent_positions):
         #TODO: take multiple agent positions, reveal the map in their lines of sight and return the grid according to their team's lines of sight. -1: unseen tile, 0: seen tile,
@@ -231,7 +275,7 @@ class VipGame(gym.Env):
         return self.grid
             
 
-    def render(self, cell_size=20):
+    def render(self, grid, cell_size=20):
         # Render the current state of the grid using Pygame
         if not self.pygame_initialized:
             # Initialize Pygame if not already initialized
@@ -247,16 +291,20 @@ class VipGame(gym.Env):
         for i in range(self.grid_height):
             for j in range(self.grid_width):
                 # Determine the color for each cell
-                if self.grid[i,j] == VIP:
+                if grid[i,j] == VIP:
                     color = (0, 255, 0)  # VIP - Green
-                elif self.grid[i,j] == DEFENDER:
+                elif grid[i,j] == DEFENDER:
                     color = (0, 0, 255)  # Defender - Blue
-                elif self.grid[i,j] == ATTACKER:
+                elif grid[i,j] == ATTACKER:
                     color = (255, 0, 0)  # Attacker - Red
-                elif self.grid[i, j] == WALL:
+                elif grid[i, j] == WALL:
                     color = (0, 0, 0)  # Wall - Gray
-                else:
+                elif grid[i, j] == SELF:
+                    color = (255, 255, 0)
+                elif grid[i, j] == UNSEEN:
                     color = (150, 150, 150)  # Empty space - Light gray
+                else:
+                    color = (255, 255, 255)
 
                 # Draw the cell
                 pygame.draw.rect(self.screen, color, pygame.Rect(j * cell_size, i * cell_size, cell_size, cell_size))
--- main.py
+++ main.py
@@ -11,24 +11,29 @@ def individual_state(state, position): #use this function to generate a state su
     return state
 
 if(__name__ == "__main__"):
-    grid_map = np.loadtxt("map_presets/grid.csv", delimiter=",")
+    grid_map = np.loadtxt("map_presets/killing_test.csv", delimiter=",")
     env = environment.VipGame(grid_map=grid_map)
-    done = False
+    truncated = False
+    terminated = False
     pygame_initialized = False
     screen = None
-    number_of_attackers = 2
-    number_of_defenders = 1
-    number_of_vips = 1    
+
     
-    while not done:
-        attacker_actions = [random.randint(0, env.attacker_defender_action_space - 1) for _ in range(number_of_attackers)]
-        defender_actions = [random.randint(0, env.attacker_defender_action_space - 1) for _ in range(number_of_defenders)]
-        vip_actions = [random.randint(0, env.vip_action_space - 1) for _ in range(number_of_vips)]
+    while not (truncated or terminated):
+        attacker_actions = [random.randint(0, env.attacker_defender_action_space - 1) for _ in range(env.number_of_attackers)]
+        defender_actions = [random.randint(0, env.attacker_defender_action_space - 1) for _ in range(env.number_of_defenders)]
+        vip_actions = [random.randint(0, env.vip_action_space - 1) for _ in range(env.number_of_vips)]
         total_actions = [attacker_actions, defender_actions, vip_actions]
-        fully_visible_state, team_states, team_rewards, team_positions, done = env.step(total_actions) #fully_visible is a single state, team_states is a list of states, team_rewards is a list of rewards, team_positions is a list of list of tuples, done is a boolean
-        print(team_rewards)
-        env.render()
+        fully_visible_state, team_states, team_rewards, team_positions, truncated, terminated = env.step(total_actions) #fully_visible is a single state, team_states is a list of states, team_rewards is a list of rewards, team_positions is a list of list of tuples, truncated is a boolean
+        print(env.live_defenders)
+        env.render(fully_visible_state)
         time.sleep(0.05)  # Wait for 100ms
+    if env.number_of_attacker_dead == env.number_of_attackers:
+        print("Defender Wins")
+    elif env.number_of_vip_dead == env.number_of_vips:
+        print("Attacker Wins")
+    else:
+        print("Draw")
 
     # env.render_line_of_sight(vip_line_of_sight)
     # env.render_line_of_sight(defender_line_of_sight)
